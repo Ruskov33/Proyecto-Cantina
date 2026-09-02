eb26964964e4a1a5758928690f8f6334c7dfdb97
ballarionahuel <ballarionahuel@gmail.com>
2026-09-02 14:30:48 -0300

    Adapatación para SQLite

--- BD_VOUCHER_T1.sql
+++ BD_VOUCHER_T1.sql
@@ -1,53 +1,47 @@
 
-DROP DATABASE if EXISTS VOUCHER;
-
-CREATE DATABASE VOUCHER;
-
-USE VOUCHER;
 --Datos basicos de alumnos
 CREATE TABLE ALUMNOS (
-  id_alumno INT AUTO_INCREMENT PRIMARY KEY,
-  Codigo CHAR(3) NOT NULL UNIQUE,
-  DNI VARCHAR(10) NOT NULL UNIQUE,
-  Nombre VARCHAR(100) NOT NULL,
-  Apellido VARCHAR(100) NOT NULL,
-  Curso CHAR(2) NOT NULL,
-  Division CHAR(2) NOT NULL,
-  Turno VARCHAR(10) NOT NULL, 
-  Activo BOOLEAN DEFAULT TRUE -- Para no tener que eliminar un registro entero en caso de darse de baja temporalmente
+  id_alumno INTEGER AUTOINCREMENT PRIMARY KEY,
+  Codigo TEXT NOT NULL UNIQUE,
+  DNI TEXT NOT NULL UNIQUE,
+  Apellido TEXT NOT NULL,
+  Curso TEXT NOT NULL,
+  Division TEXT NOT NULL,
+  Turno TEXT NOT NULL, 
+  Activo INTEGER DEFAULT 1 -- Para no tener que eliminar un registro entero en caso de darse de baja temporalmente
 );
 
 -- Usamos para definir y referenciar el nombre de la comida, pueden modificarse. En principio serian solo 4
 CREATE TABLE TIPO_VOUCHER (
-  id_tipo_voucher INT AUTO_INCREMENT PRIMARY KEY,
-  Nombre_comida VARCHAR(50) NOT NULL
+  id_tipo_voucher INTEGER AUTOINCREMENT PRIMARY KEY,
+  Nombre_comida TEXT NOT NULL
 );
 
 -- Tabla de asignacion para cada alumna
 CREATE TABLE ASIGNACION_VOUCHER (
-  id_asignacion INT AUTO_INCREMENT PRIMARY KEY,
-  id_alumno INT, -- FK
-  id_tipo_vaucher INT, -- FK
-  dia_semana INT NOT NULL,
+  id_asignacion INTEGER AUTOINCREMENT PRIMARY KEY,
+  id_alumno INTEGER NOT NULL, -- FK
+  id_tipo_vaucher INTEGER NOT NULL, -- FK
+  dia_semana INTEGER NOT NULL,
 
   -- Referencias
   FOREIGN KEY (id_alumno) REFERENCES ALUMNOS (id_alumno),
-  FOREIGN KEY (id_tipo_vaucher) REFERENCES TIPO_VOUCHER (id_tipo_voucher),
+  FOREIGN KEY (id_tipo_voucher) REFERENCES TIPO_VOUCHER (id_tipo_voucher),
   -- No permite que haya un alumno con exactamente el mismo voucher asgiando
   CONSTRAINT uq_asignacion_unica UNIQUE (id_alumno, id_tipo_voucher, dia_semana) 
 );
 
 -- Tabla de chequeos
-CREATE TABLE Consumo_Vouchers (
-  id_consumo INT AUTO_INCREMENT PRIMARY KEY, 
-  id_alumno INT NOT NULL, -- FK
-  id_tipo_voucher INT NOT NULL, -- FK
-  fecha DATE NOT NULL, -- Para chequiar
-  hora_consumo TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Hora actual para registrar el consumo
+CREATE TABLE CONSUMO_VOUCHERS (
+  id_consumo INTEGER AUTO_INCREMENT PRIMARY KEY, 
+  id_alumno INTEGER NOT NULL, -- FK
+  id_tipo_voucher INTEGER NOT NULL, -- FK
+  fecha TEXT NOT NULL, -- Para chequiar
+  hora_consumo TEXT DEFAULT CURRENT_TIMESTAMP, -- Hora actual para registrar el consumo
     
   -- Referencias a tablas foraneas
-  FOREIGN KEY (id_alumno) REFERENCES Alumnos(id_alumno),
-  FOREIGN KEY (id_tipo_voucher) REFERENCES Tipos_Voucher(id_tipo_voucher),
+  FOREIGN KEY (id_alumno) REFERENCES ALUMNOS(id_alumno),
+  FOREIGN KEY (id_tipo_voucher) REFERENCES TiPO_VOUCHER(id_tipo_voucher),
     
   -- No permite acumular consumos con los tres campos indenticos
    CONSTRAINT uq_consumo_unico_por_dia UNIQUE (id_alumno, id_tipo_voucher, fecha)
