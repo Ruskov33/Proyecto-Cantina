3c95a75c3daa48670a5f9a0749a3fc9980984403
ballarionahuel <ballarionahuel@gmail.com>
2026-09-03 19:26:57 +0000

    UPDATE 1

--- gestorBD.py
+++ gestorBD.py
@@ -1,5 +1,33 @@
 import sqlite3
 
-/*
-  Aca vamos a converitr el pseucodigo a py con sqlite para gstionar la bd.
-*\  
+conexion = sqlite3.connect("BD_VOUCHER_T1.db")
+
+cursor = conexion.cursor()
+
+cursor.execute("SELECT * FROM TIPO_VOUCHER")
+
+resultados = cursor.fetchall()
+
+import sqlite3
+
+conexion = sqlite3.connect("BD_VOUCHER_T1.db")
+
+cursor = conexion.cursor()
+
+comidas = [
+    ("Desayuno",),
+    ("Almuerzo",),
+    ("Merienda",),
+    ("Cena",)
+]
+
+cursor.executemany("""
+    INSERT INTO TIPO_VOUCHER (Nombre_comida)
+    VALUES (?)
+""", comidas)
+
+conexion.commit()
+
+print("Comidas cargadas correctamente")
+
+conexion.close()
