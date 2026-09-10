be9d1a2e9fd82ddc5c40c507de3b19b607b4ad4c
Gafa <facugasparini@gmail.com>
2026-09-10 17:13:22 -0300

    Comentarios Java Script

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//Declaracion de constantes
 const iniciarBtn = document.getElementById('iniciarBtn');
 const content = document.querySelector('.content');
 const salirBtn = document.getElementById('salirBtn');
 const manualBtn = document.getElementById('manualBtn');
 const NEXT_PAGE = 'menu.html';
 
+//Pasar pagina con el boton INICIAR
 iniciarBtn.addEventListener('click', () => {
     content.classList.add('slide-out');
     
@@ -12,17 +14,19 @@ iniciarBtn.addEventListener('click', () => {
     }, { once: true });
 });
 
+//Cerrar pagina con el boton de SALIR
 salirBtn.addEventListener('click', () => {
     window.close();
     console.log('SALIR presionado');
 });
 
+//Pasar pagina con el boton de MANUAL
 manualBtn.addEventListener('click', () => {
     window.location.href = 'manual.html';
     console.log('MANUAL presionado');
 });
 
-// --- SOLUCIÓN PARA EL BFCache ---
+//SOLUCIÓN PARA EL BFCache
 window.addEventListener('pageshow', (event) => {
     // Si la página se está cargando desde el caché (botón 'Atrás') 
     // o si el contenedor ya tiene la clase aplicada, la removemos.
